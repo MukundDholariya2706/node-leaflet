99983ebe6552b13ef7ad9a65fc6e4d34cb9a0008
Mukund Dholariya <mukunddtridhyatech@gmail.com>
2024-04-19 12:51:23 +0530

    hello

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ app.post("/update-location", (req, res) => {
   res.json({ status: 200 });
 });
 
+app.post("/", (req, res) => {
+  // trigger a new post event via pusher
+  res.json({ status: 200 }).send('Hello');
+});
+
 app.listen(process.env.PORT, () => {
   console.log("server is running");
 });
